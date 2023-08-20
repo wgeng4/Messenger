3e3ca1fa6a946e5ce25ffd57bd391a73bcf01254
Fumi G <wgeng1@uci.edu>
2023-08-19 17:33:57 -0700

    Auth UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -52,8 +56,61 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm: rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-
+          {variant == 'REGISTER' && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input id="email" label="Email address" type="email" register={register} errors={errors} />
+          <Input id="password" label="Password" type="password" register={register} errors={errors} />
+          <div>
+            <Button
+              disabled={isLoading}
+              fullWidth
+              type="submit"
+            >
+              {variant == 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="
+                absolute
+                inset-0
+                flex
+                items-center
+              ">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or contine with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant == 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+          </div>
+          <div onClick={toggleVariant}
+            className="underline cursor-pointer"
+          >
+            {variant == 'LOGIN' ? 'Create and account':'Log in'}
+          </div>
+        </div>
       </div>
     </div>
   )
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "./components/AuthForm"
 
 export default function Home() {
   return (
-    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-200">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           src={"/images/logo.png"}
