0aa73d614b63c9018a39ed9dbfe3695c19b08a0b
Fumi G <wgeng1@uci.edu>
2023-08-21 16:16:10 -0700

    feat: reg

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from 'react-icons/bs'
+import axios from "axios";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -38,7 +39,7 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant == 'REGISTER') {
-      // Axios register
+      axios.post('/api/register', data)
     }
 
     if (variant == 'LOGIN') {
