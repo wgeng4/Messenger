12ad1c3579757f8ce02e86f691465d094d01d306
Fumi G <wgeng1@uci.edu>
2023-08-24 17:38:41 -0700

    feat: nav and sidebar

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+async function Sidebar({ children }: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='h-full'>
+      {children}
+    </div>
+  )
+}
+
+export default Sidebar;
