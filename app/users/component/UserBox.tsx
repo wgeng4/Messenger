d90f132c750598d7a1de44bb8a0d37d12ac20ae9
Fumi G <wgeng1@uci.edu>
2023-09-01 17:21:50 -0700

    conversation router

--- app/users/component/UserBox.tsx
+++ app/users/component/UserBox.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Avatar from "@/app/components/Avatar"
 import { User } from "@prisma/client"
 import axios from "axios"
 import { useRouter } from "next/navigation"
@@ -28,7 +29,33 @@ const UserBox: React.FC<UserBoxProps> = ({
 
 
   return (
-    <div>UserBox</div>
+    <div
+      onClick={handleClick}
+      className="
+    w-full 
+    relative 
+    flex 
+    items-center 
+    space-x-3 
+    bg-white 
+    p-3 
+    hover:bg-neutral-100
+    rounded-lg
+    transition
+    cursor-pointer
+  ">
+      <Avatar user={data} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <span className="absolute inset-0" aria-hidden="true" />
+          <div className="flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">
+              {data.name}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
